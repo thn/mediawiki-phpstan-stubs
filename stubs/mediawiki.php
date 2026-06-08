8c8fa177c165b531d3e81fe2f4d978fdd63c3845
Peter Hatton <pete@monolight.org>
2026-06-08 17:07:14 +0000

    More stubs for templates

--- stubs/mediawiki.php
+++ stubs/mediawiki.php
@@ -12,6 +12,51 @@ namespace {
         public function getContext(): \IContextSource {}
     }
 
+    class SkinTemplate
+    {
+        public string $skinname;
+        public string $stylename;
+        public string $template;
+
+        public function __construct(string $skinname) {}
+        public function initPage(\MediaWiki\Output\OutputPage $out): void {}
+        /** @param mixed $classname @param mixed $repository @param mixed $cache_dir @return mixed */
+        public function setupTemplate(mixed $classname, mixed $repository = false, mixed $cache_dir = false): mixed {}
+        public function getUser(): \User {}
+        public function getTitle(): \MediaWiki\Title\Title {}
+        public function getRelevantTitle(): \MediaWiki\Title\Title {}
+        /** @return array<string, mixed> */
+        public function getFooterIcons(): array {}
+        /** @param array<string, mixed> $icon */
+        public function makeFooterIcon(array $icon): string {}
+        public function getOutput(): \MediaWiki\Output\OutputPage {}
+    }
+
+    class BaseTemplate
+    {
+        /** @var array<string, mixed> */
+        public array $data;
+        public \MediaWiki\Config\Config $config;
+
+        public function getSkin(): \SkinTemplate {}
+        public function html(string $key): void {}
+        public function text(string $key): void {}
+        public function msg(string $key): void {}
+        public function getMsg(string $key): \Message {}
+        /** @param array<string, mixed> $val */
+        public function makeListItem(string $key, array $val): string {}
+        /** @param array<string, mixed> $options */
+        public function makeSearchInput(array $options = []): string {}
+        public function makeSearchButton(string $mode, array $options = []): string {}
+        /** @return array<string, mixed> */
+        public function getPersonalTools(): array {}
+        /** @return array<string, array<int, string>> */
+        public function getFooterLinks(): array {}
+        /** @return mixed */
+        public function get(string $key): mixed {}
+        public function getIndicators(): string {}
+    }
+
     interface IContextSource
     {
         public function getWikiPage(): \WikiPage;
@@ -33,6 +78,7 @@ namespace {
     class User
     {
         public function isAllowed(string $permission): bool {}
+        public function isRegistered(): bool {}
     }
 
     class WebRequest
@@ -108,6 +154,8 @@ namespace {
     {
         public function inContentLanguage(): self {}
         public function text(): string {}
+        public function exists(): bool {}
+        public function escaped(): string {}
     }
 
     function wfMessage(string $key, mixed ...$params): \Message {}
@@ -145,6 +193,16 @@ namespace MediaWiki\Title {
         public function getLocalURL(): string {}
         public function getPrefixedURL(): string {}
         public function getArticleID(): int {}
+        public function getPageViewLanguage(): \Language {}
+    }
+
+}
+
+namespace {
+
+    class Language
+    {
+        public function getHtmlCode(): string {}
     }
 
 }
@@ -156,6 +214,111 @@ namespace MediaWiki {
         public static function getInstance(): self {}
         public function getConnectionProvider(): \Wikimedia\Rdbms\IConnectionProvider {}
         public function getRepoGroup(): \RepoGroup {}
+        public function getConfigFactory(): \MediaWiki\Config\ConfigFactory {}
+        public function getHookContainer(): \MediaWiki\HookContainer\HookContainer {}
+        public function getWatchlistManager(): \MediaWiki\Watchlist\WatchlistManager {}
+        public function getGroupPermissionsLookup(): \MediaWiki\Permissions\GroupPermissionsLookup {}
+    }
+
+}
+
+namespace MediaWiki\Output {
+
+    class OutputPage
+    {
+        public function addMeta(string $name, string $content): void {}
+        /** @param string|string[] $modules */
+        public function addModuleStyles(mixed $modules): void {}
+        public function addModules(string ...$modules): void {}
+        public function addHTML(string $html): void {}
+        public function getTitle(): \MediaWiki\Title\Title {}
+        public function headElement(\SkinTemplate $skin): string {}
+    }
+
+}
+
+namespace MediaWiki\Config {
+
+    interface Config
+    {
+        /** @return mixed */
+        public function get(string $name): mixed;
+        public function has(string $name): bool;
+    }
+
+    class ConfigFactory
+    {
+        public function makeConfig(string $name): \MediaWiki\Config\Config {}
+    }
+
+}
+
+namespace MediaWiki\Html {
+
+    class Html
+    {
+        /** @param array<string, mixed> $attribs */
+        public static function rawElement(string $element, array $attribs = [], string $contents = ''): string {}
+        /** @param array<string, mixed> $attribs */
+        public static function hidden(string $name, string $value, array $attribs = []): string {}
+    }
+
+}
+
+namespace MediaWiki\Linker {
+
+    class Linker
+    {
+        public static function tooltip(string $name, ?string $options = null): string {}
+        /** @param array<string, mixed> $msgParams @return array<string, string> */
+        public static function tooltipAndAccesskeyAttribs(string $name, array $msgParams = [], ?string $options = null): array {}
+    }
+
+}
+
+namespace MediaWiki\Parser {
+
+    class Sanitizer
+    {
+        public static function escapeIdForAttribute(string $id, int $mode = 0): string {}
+    }
+
+}
+
+namespace MediaWiki\Xml {
+
+    class Xml
+    {
+        /** @param array<string, string>|null $attribs */
+        public static function expandAttributes(?array $attribs): string {}
+    }
+
+}
+
+namespace MediaWiki\HookContainer {
+
+    class HookContainer
+    {
+        /** @param mixed[] $args */
+        public function run(string $hook, array $args = []): bool {}
+    }
+
+}
+
+namespace MediaWiki\Watchlist {
+
+    class WatchlistManager
+    {
+        public function isWatched(\User $user, \MediaWiki\Title\Title $title): bool {}
+    }
+
+}
+
+namespace MediaWiki\Permissions {
+
+    class GroupPermissionsLookup
+    {
+        public function groupHasPermission(string $group, string $permission): bool {}
     }
 
 }
